c682779298f789bec7c00b16d2948af859352a4f
Nicolas <nicrichard94@gmail.com>
2013-03-31 16:07:03 -0400

    Ajout de commentaires aux classes Bloc et Carte

--- BBTA/BBTA/Classe/Carte/Carte.cs
+++ BBTA/BBTA/Classe/Carte/Carte.cs
@@ -11,43 +11,67 @@ using BBTA.Elements;
 namespace BBTA
 {
     /// <summary>
-    /// La classe contient
+    /// La classe contient les blocs composant le relief ainsi que l'arrière-plan de la carte.
+    /// -----------------------------------------------------------------------------------------------
+    /// Affiche les blocs nécessaire.
+    /// Affiche l'arrière-plan.
+    /// Gère la destruction des blocs s'il y a lieu.
+    /// -----------------------------------------------------------------------------------------------
     /// </summary>
     public class Carte
     {
+        //Variables-----------------------------------------------------------------------------------------------
         private Texture2D textureArrierePlan;
-        private Texture2D texturesBlocs;
-        //Un autre conflit de merging provoqué
-        private const float TAILLE_BLOC = 1f;
         private Bloc[] blocs;
-        public Carte(int[] donneesBlocs, int largeurCarte, Texture2D arrierePlan, Texture2D texturesBlocs, World mondePhysique, float MetrePixel)
+
+        //Constantes----------------------------------------------------------------------------------------------
+        private const float TAILLE_BLOC = 1f;
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="donneesBlocs">Données sur la nature des blocs</param>
+        /// <param name="largeurCarte">Largeur de la carte (en blocs)</param>
+        /// <param name="arrierePlan">Arrière-plan de la carte</param>
+        /// <param name="textureBlocs">Texture des blocs</param>
+        /// <param name="mondePhysique">World Farseer</param>
+        /// <param name="MetrePixel">Valeur en pixel d'un metre</param>
+        public Carte(int[] donneesBlocs, int largeurCarte, Texture2D arrierePlan, Texture2D textureBlocs, World mondePhysique, float MetrePixel)
         {
             this.textureArrierePlan = arrierePlan;
-            this.texturesBlocs = texturesBlocs;
-
             blocs = new Bloc[donneesBlocs.Length];
             for(int compteurBlocs = 0; compteurBlocs < donneesBlocs.Length; compteurBlocs++)
             {
+                //Par convention, une case avec comme donnée "0" signifie une case vide.  En somme, il n'y a aucun bloc.
                 if(donneesBlocs[compteurBlocs] != 0)
                 {
                     Vector2 positionBloc = new Vector2(compteurBlocs%largeurCarte*MetrePixel, compteurBlocs/largeurCarte*MetrePixel);
-                    blocs[compteurBlocs] = new Bloc(mondePhysique, positionBloc, texturesBlocs, TAILLE_BLOC);
+                    blocs[compteurBlocs] = new Bloc(mondePhysique, positionBloc, textureBlocs, TAILLE_BLOC);
                 }                
             }
         }
 
+        /// <summary>
+        /// Détruit les blocs nécessaires suite à une explosion
+        /// </summary>Lieu d'origine de l'explosion</param>
+        /// <param name="rayon">Rayon de l'explosion</param>
+        /// <param name="puissance">Puissance déployée par l'explosion</param>
         public void Explosion(Vector2 lieu, float rayon, float puissance)
         {
-            float pente = -puissance/rayon;
             for (int compteurBloc = 0; compteurBloc < blocs.Length; compteurBloc++)
 	        {
                 if(blocs[compteurBloc].Explosetil(puissance, rayon, lieu))
                 {
+                    //Destruction du bloc
                     blocs[compteurBloc] = null;
                 }
 	        }
         }
 
+        /// <summary>
+        /// Affiche les blocs nécessaires à l'écran
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(textureArrierePlan, Vector2.Zero, Color.White);
@@ -55,7 +79,7 @@ namespace BBTA
             {
                 if (item != null)
                 {
-                    spriteBatch.Draw(texturesBlocs, item.Position, Color.White);
+                    item.Draw(spriteBatch);
                 }
             }
         }
