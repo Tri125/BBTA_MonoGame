80636da7c7aac61c79df6e1d63b0033bf20330fb
Tristan <kraig.tri125@gmail.com>
2013-04-12 10:29:48 -0400

    Un nouveau dossier et deux nouvelles classes. Ils seront utilisées pour enregistrer et charger les paramètres de l'utilisateur(les options).

---
+++ BBTA/BBTA/Classe/Option/Option.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BBTA.Classe.Option
+{
+    [Serializable]
+    public class Option
+    {
+    }
+}
