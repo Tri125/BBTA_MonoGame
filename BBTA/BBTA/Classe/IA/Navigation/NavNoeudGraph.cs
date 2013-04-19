a4514850edeb61b33c116af2b464793a5cf59c4a
Tristan <kraig.tri125@gmail.com>
2013-04-18 21:52:35 -0400

    Deux nouvelles classes qui seront utilisées pour le graph de navigation.

---
+++ BBTA/BBTA/Classe/IA/Navigation/NavNoeudGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BBTA.Classe.IA.Navigation
+{
+    public class NavNoeudGraph : NoeudGraph
+    {
+        private Vector2 position;
+    }
+}
