d9c87463631e56550261d94bec6570e911410913
Tristan <kraig.tri125@gmail.com>
2013-04-19 20:30:44 -0400

    Essais tant bien que mal à traduire cette classe compliqué qui défini le graphe au complet. On utilise des Lists dans des LinkedList, mal à la tête et je ne suis même pas certain comment cela fonctionne en pratique. Rajouté une méthode qui supposément devrais vérifier si un arc est unique, mais je ne parirais pas ma vie dessus. Une petite lecture sur les LinkedList devrait régler le mal de tête.

--- BBTA/BBTA/Classe/IA/Navigation/Graphe.cs
+++ BBTA/BBTA/Classe/IA/Navigation/Graphe.cs
@@ -7,5 +7,34 @@ namespace BBTA.Classe.IA.Navigation
 {
     public class Graphe
     {
+        //Liste des noeuds fesant partie du graphe
+        private List<NavNoeudGraph> NoeudGraphe;
+
+        private LinkedList<NavArcGraph> LienArc;
+
+        //Une liste d'arc adjacent. L'index d'un noeud est relié avec les arcs de cette liste
+        private List<LinkedList<NavArcGraph>> ArcAdjacent;
+        //Est-ce un graphe orienté?
+        private bool graphe_orienter;
+        //L'index du prochain noeud à être rajouté dans la liste
+        private int prochainIndex;
+
+
+        //-------------------------------- UniqueEdge ----------------------------
+        //
+        //  returns true if the edge is not present in the graph. Used when adding
+        //  edges to prevent duplication
+        //------------------------------------------------------------------------
+        private bool ArcUnique(int debut, int fin)
+        {
+            foreach (var item in ArcAdjacent[debut])
+            {
+                if (item.IndexDest == fin)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
