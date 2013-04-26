c718fca2450d8e61bdc1155c1cc2478d94cae3d2
Tristan <kraig.tri125@gmail.com>
2013-04-26 13:41:13 -0400

    Constructeur dans Graphe.cs. Class teste Trash pour graph

---
+++ BBTA/BBTA/Classe/Outils/TesteGraphe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BBTA.Classe.IA.Navigation;
+using Microsoft.Xna.Framework.Input;
+
+namespace BBTA.Classe.Outils
+{
+    public class TesteGraphe
+    {
+        public Graphe Graph;
+
+        public TesteGraphe()
+        {
+            Graph = new Graphe(false);
+        }
+    }
+}
