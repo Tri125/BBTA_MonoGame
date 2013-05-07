dc896b0511bd1c29ada739ee83d7ba3f81ed9a7c
Philippe <philjan_patteblanche@hotmail.com>
2013-05-06 21:31:11 -0400

    Modification classe MenuFinDePartie.cs

--- BBTA/BBTA/Classe/Menus/MenuFinDePartie.cs
+++ BBTA/BBTA/Classe/Menus/MenuFinDePartie.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BBTA.Classe.Menus
 {
     public class MenuFinDePartie : MenuArrierePlan
     {
+        private Texture2D lettrage;
         private EtatJeu prochainEtat;
+        private Color equipePerdante;
 
         public MenuFinDePartie(Game game, Color equipePerdante)
             : base(game)
         {
             prochainEtat = EtatJeu.FinDePartie;
+            this.equipePerdante = equipePerdante;
         }
+
 
         //Color.Firebrick
         //Color.Blue
