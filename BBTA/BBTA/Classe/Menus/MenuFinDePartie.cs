ddb58761bb0e93b8eb4bd4632fe38adaac6f1a48
Philippe <philjan_patteblanche@hotmail.com>
2013-05-07 08:52:19 -0400

    Ajout LettrageMenuFinDePartie

--- BBTA/BBTA/Classe/Menus/MenuFinDePartie.cs
+++ BBTA/BBTA/Classe/Menus/MenuFinDePartie.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using BBTA.Interface;
+using IndependentResolutionRendering;
 
 namespace BBTA.Classe.Menus
 {
     public class MenuFinDePartie : MenuArrierePlan
     {
         private Texture2D lettrage;
+        private Bouton btnAccueil;
         private EtatJeu prochainEtat;
         private Color equipePerdante;
 
@@ -18,6 +21,19 @@ namespace BBTA.Classe.Menus
         {
             prochainEtat = EtatJeu.FinDePartie;
             this.equipePerdante = equipePerdante;
+        }
+
+        protected override void LoadContent()
+        {
+            lettrage = Game.Content.Load<Texture2D>(@"Ressources\Menus\FinDePartie\LettrageFinDePartie");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, null, null, null, null, Resolution.getTransformationMatrix());
+            spriteBatch.Draw(lettrage, Vector2.Zero, Color.White);
+            spriteBatch.End();
         }
 
 
