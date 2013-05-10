c1778c1c8612e4657aedf766c8e2b132a6228e9b
Tristan <kraig.tri125@gmail.com>
2013-05-09 21:56:04 -0400

    Intégration du projet d'éditeur de niveau dans le projet BBTA. Faire attention, bien que certaines classes sont semblables à 99.99%, une petite modification personalisé a été fait pour l'éditeur.

---
+++ EditeurBBTA/Classe/Camera.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EditeurCarteXNA
+{
+    public class Camera
+    {
+        protected float zoom;
+        protected Matrix transform;
+        public Matrix ViewMatrix {get {return transform;} }
+        public Vector2 Position { get; protected set; }
+        public float Rotation { get; set; }
+
+        public Camera()
+        {
+            zoom = 1.0f;
+            Rotation = 0.0f;
+            Position = Vector2.Zero;
+        }
+
+        public Camera(Vector2 position)
+            : this()
+        {
+            Position = position;
+        }
+
+        public float Zoom
+        {
+            get { return zoom; }
+            set
+            {
+                zoom = value;
+                if (zoom < 0.1f)
+                    zoom = 0.1f;
+            }
+        }
+
+
+        public void Move(Vector2 displacement)
+        {
+            Position += displacement;
+        }
+
+        public Matrix CreateTransformationMatrix(GraphicsDevice device)
+        {
+            transform = Matrix.CreateTranslation(new Vector3(-Position.X, -Position.Y, 0)) *
+                                                  Matrix.CreateRotationZ(Rotation) *
+                                                  Matrix.CreateScale(new Vector3(zoom, zoom, 1)) *
+                                                  Matrix.CreateTranslation(new Vector3(device.Viewport.Width * 0.5f, device.Viewport.Height * 0.5f, 0));
+
+            return transform;
+        }
+
+    }
+}
---
+++ EditeurBBTA/Classe/TuileEditeur.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EditeurCarteXNA
+{
+    /// <summary>
+    ///La classe TuileEditeur est utilisé pour l'éditeur de niveau dans le but de créer un fichier carte.
+    ///Chaque tuile a un  int qui identifie le type de la tuile et un Rectangle pour cliquer dessus et l'afficher dans l'éditeur.
+    /// </summary>
+    public class TuileEditeur
+    {
+        private Rectangle bloc;
+        private int idTuile;
+
+        public int IdTuile
+        {
+            get
+            {
+                return idTuile;
+            }
+            set
+            {
+                idTuile = value;
+            }
+        }
+
+        public Rectangle Bloc
+        {
+            get
+            {
+                return bloc;
+            }
+        }
+
+        /// <summary>
+        /// Constructeur de base de TuileEditeur.
+        /// La classe contient un identifiant et un Rectangle. Utilisé dans l'editeur de niveau.
+        /// </summary>
+        public TuileEditeur()
+        {
+            idTuile = 0;
+        }
+
+        /// <summary>
+        /// Constructeur de TuileEditeur.
+        /// La classe contient un identifiant et un Rectangle. Utilisé dans l'editeur de niveau.
+        /// </summary>
+        /// <param name="bloc">
+        /// Rectangle bloc définie la zone occupé par la tuile dans l'éditeur de niveau.
+        /// </param>
+        /// <param name="idTuile">
+        /// int idTuile est l'identifiant numérique représentant le type de la tuile.
+        /// </param>
+        public TuileEditeur(Rectangle bloc, int tileID = 0)
+        {
+            this.bloc = bloc;
+            this.idTuile = tileID;
+        }
+    }
+}
